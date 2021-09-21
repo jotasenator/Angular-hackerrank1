e439ab3e6a631deafd7f6f051f557a2d6f545d28
jotas <jotasenator@gmail.com>
2021-09-21 16:03:23 -0400

    Hasta aqui con la primera app

---
+++ src/app/temperature-converter/temperature-converter.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-temperature-converter',
+  templateUrl: './temperature-converter.component.html',
+  styleUrls: ['./temperature-converter.component.css']
+})
+export class TemperatureConverterComponent implements OnInit {
+
+  isNaN: Function = Number.isNaN
+  parseFloat: Function = Number.parseFloat
+
+  C: string = ''
+  F: string = ''
+
+  onGradeKey(event: any) {
+    this.C = event.target.value;
+  }
+  onFarenheitKey(event: any) {
+    this.F = event.target.value;
+  }
+
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void { }
+
+  handleChangeGrades = () => (
+
+    ((+(this.F) - 32) * 5 / 9).toFixed(1)
+  )
+  handleChangeFarenheit = () => (
+
+
+    (+(this.C) * 9 / 5 + 32).toFixed(1)
+  )
+
+
+}
